a8c10ced7ca8b8626707c8476cca067eb9565c3d
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-05-26 17:39:39 +0500

    test

--- models/route.js
+++ models/route.js
@@ -4,12 +4,8 @@ const routeSchema = new mongoose.Schema({
     uz_name: String,
     ru_name: String,
     en_name: String,
-    uz_from: String,
-    en_from: String,
-    ru_from: String,
-    uz_to: String,
-    ru_to: String,
-    en_to: String,
+    from: String,
+    to: String,
     trips: [{type: mongoose.Schema.Types.ObjectId, ref: "Trip"}]
 }, { timestamps: true })
 
