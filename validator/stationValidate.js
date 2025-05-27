8afdd91c7bc5637487a9ee27d466c2c1b6e3c574
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-05-27 15:32:56 +0500

    test

--- validator/stationValidate.js
+++ validator/stationValidate.js
@@ -1,21 +1,5 @@
 exports.stationCreateSchema = {
-    uz_name: {
-        isString: {
-            errorMessage: "Bekat nomi string bo'lishi kerak!"
-        },
-        notEmpty: {
-            errorMessage: "Bekat nomi bo'sh bo'lmasligi kerak!"
-        }
-    },
-    ru_name: {
-        isString: {
-            errorMessage: "Bekat nomi string bo'lishi kerak!"
-        },
-        notEmpty: {
-            errorMessage: "Bekat nomi bo'sh bo'lmasligi kerak!"
-        }
-    },
-    en_name: {
+    name: {
         isString: {
             errorMessage: "Bekat nomi string bo'lishi kerak!"
         },
